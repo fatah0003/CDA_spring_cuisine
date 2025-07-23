ebe44adafba5362ff65f2e4412e963a5004ae989
fatah0003 <fatahutd@gmail.com>
2025-07-23 16:19:42 +0200

    add entities

--- src/main/java/org/example/exo_4_cuisine/model/Categorie.java
+++ src/main/java/org/example/exo_4_cuisine/model/Categorie.java
@@ -1,16 +1,22 @@
 package org.example.exo_4_cuisine.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
 import java.util.UUID;
-
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String nom;
     private String description;
+    @OneToMany(mappedBy = "categorie")
+    private List<Recette> recettes;
 }
--- src/main/java/org/example/exo_4_cuisine/model/Recette.java
+++ src/main/java/org/example/exo_4_cuisine/model/Recette.java
@@ -1,20 +1,28 @@
 package org.example.exo_4_cuisine.model;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@Builder
 public class Recette {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String nom;
     private String ingredient;
     private String instruction;
+    @ManyToOne
+    @JoinColumn(name = "categorie_id")
     private Categorie categorie;
+
 
 }
